3b938e6d7714aa8c5c135c224f76867768964b5c
sebastien.rey-coyrehourcq <s.rey.coyrehourcq@proton.me>
2023-02-25 22:32:11 +0100

    init RPI config repository

---
+++ minicran/utils/apache2.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+apt install -y apache2
+
+cp ../ApacheConfFiles/apache2.conf /etc/apache2/
+
+#enable websites
+#for i in $(ls ../ApacheConfFiles/tim.*) ; do
+#    cp $i /etc/apache2/sites-available
+#    shortname=$(basename $i)
+#    mkdir /var/www/$(echo $shortname | sed 's/.conf//')
+#    a2ensite $shortname
+#done
+
+cp -r ../html/. /var/www/html/
+
+apachectl configtest
+systemctl enable apache2
+#enable server side includes
+a2enmod include
+systemctl restart apache2
